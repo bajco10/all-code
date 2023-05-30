0ecbb5088fdfa47d263e74e27a5faf0d4b827b6d
bajco10 <tomasbajana10@gmail.com>
2023-05-30 11:21:21 +0200

    added alternative solution to the old phone problem

--- SCHOOL/hodiny/30.5.23.py
+++ SCHOOL/hodiny/30.5.23.py
@@ -1,3 +1,4 @@
+# overkill riesenie
 def kodovanie(retazec):
     l = [[" "], ["a", "b", "c"], ["d", "e", "f"], ["g", "h", "i"], ["j", "k", "l"], ["m", "n", "o"], ["p", "q", "r"], ["s", "t", "u"], ["v", "w","x"], ["y", "z"]]
     res = ""
@@ -7,8 +8,23 @@ def kodovanie(retazec):
             if i in j:
                 x = l.index(j)
                 n = j.index(i)
-                print(i, x+1, n+1)
                 res+=str(x)*(n+1)
     return res
 
 print(kodovanie("janko hrasko"))
+
+# riesenie cez ordinalne hodnoty
+
+def kodovanie(textik):
+    textik = textik.upper()
+    v = ""
+    for i in textik:
+        if i == " ":
+            v+= "0"
+            continue
+        poc = (ord(i)-65)%3 +1
+        cislo = str(((ord(i)-65)//3)+1)
+        v += poc*cislo
+    print(v)
+
+kodovanie("Janko Hrasko")
