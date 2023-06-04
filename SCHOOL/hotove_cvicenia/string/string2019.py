167354c2c50b9dc281e23974e377e46edc58907f
bajco10 <tomasbajana10@gmail.com>
2023-06-04 23:32:19 +0200

    19ka spravena pre hlupejsich :)

--- SCHOOL/hotove_cvicenia/string/string2019.py
+++ SCHOOL/hotove_cvicenia/string/string2019.py
@@ -165,8 +165,23 @@ for i in range(6):
     x+=50
 c.mainloop()"""
 #19 -> bolo na ulohu
-#20
+#19 riesenie len so string
 import tkinter
+c = tkinter.Canvas()
+c.pack()
+def stvorce(velkost, farby):
+    x, y = 30, 100
+    for i in range(farby.count(" ")):
+        medz = farby.find(" ")
+        c.create_rectangle(x, y, x+velkost, y-velkost, fill=f"{farby[:medz]}")
+        farby = farby[medz+1:]
+        x+=velkost+5
+    c.create_rectangle(x, y, x+velkost, y-velkost, fill=farby)
+
+stvorce(40, "red blue purple red gold")
+c.mainloop()
+#20
+"""import tkinter
 c = tkinter.Canvas(width=450)
 c.pack()
 def stvorce(retazec):
@@ -182,7 +197,7 @@ def stvorce(retazec):
             c.create_rectangle(x, y, x+int(l[i]), y-int(l[i]), fill=f"{l[int(i)+1]}")
             x+=int(l[i]) + 5
 stvorce('40 red 20 blue 60 purple 40 red 30 gold')
-c.mainloop()
+c.mainloop()"""
 #21
 """import tkinter
 
