0036451eed020f7e4ba0cc9b2014a52de44170bc
bajco10 <tomasbajana10@gmail.com>
2024-01-11 17:00:37 +0100

    dorobene zapisanie do csv suboru podla datumov

--- SCHOOL/hodiny/2023/24/9.1.2024.py
+++ SCHOOL/hodiny/2023/24/9.1.2024.py
@@ -20,11 +20,11 @@ for riadok in subor:
     if mesiac in mesiace:
         mesiace[(mesiac)] += float(suma)
     else:
-        mesiace[(mesiac)] = float(suma)"""
+        mesiace[(mesiac)] = float(suma)
 
 # print(mesiace)
 
-"""for mesiac, suma in sorted(mesiace.items(), key=lambda x:int(x[0])):
+for mesiac, suma in sorted(mesiace.items(), key=lambda x:int(x[0])):
     print(f"{mesiac}. - {round(suma, 2)}")
 print()
 for mesiac, suma in sorted(mesiace.items(), key=lambda x:x[1], reverse=True):
@@ -32,7 +32,7 @@ for mesiac, suma in sorted(mesiace.items(), key=lambda x:x[1], reverse=True):
 
 # zapisanie do suboru podla datumu
 # dorobit!
-with open("zoradene_naklady.csv", "w") as subor:
+'''with open("zoradene_naklady.csv", "w") as subor:
     pass
 
 import csv
@@ -56,6 +56,38 @@ for riadok in subor:
 for datum, riadok in sorted(sumy.items()):
     # print(datum)
     zoradene_srandy.append(riadok)
-print(zoradene_srandy)
+print(zoradene_srandy)'''
 
+import csv
+
+file = open("naklady.csv", encoding="utf-8-sig")
+subor = csv.reader(file)
+
+slov = {i: [] for i in range(1,13)}
+utried = []
+date = []
+for riadok in subor:
+    datum, hodnota = riadok[0].split()
+    d, m, r = datum.split(".")
+    str_date = f'{r}-{int(m):02}-{int(d):02}'
+    #ulozenie do slovnika kde klucom su mesiace
+    slov[int(m)].append(float(hodnota[:-1]))
+    #priprava na zapis do suboru zoredene podla datumu
+    date.append([str_date, riadok])
+
+#Vypis podla mesiacov       
+for kluc, suma in slov.items():
+    print(f'{kluc:2}: {sum(suma):.2f}€')
+    #priprava na utriedenie podla trzieb
+    utried.append([sum(suma), kluc])
+print()
+#vypis podla trzieb    
+for prvok in sorted(utried, reverse=True):
+    print(f'{prvok[1]:2}: {prvok[0]:.2f}€')
+    
+#ulozenie do suboru utriedene podla datumu
+with open('usporiadane.csv', 'w', encoding="utf-8-sig") as f:
+    writer = csv.writer(f, lineterminator='\n', delimiter=" ")
+    for riadok in sorted(date):
+        writer.writerow(riadok[1])
     
