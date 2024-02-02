c239d16acfc9fc9ca0b82b92d68d83e409a5ac42
bajco10 <tomasbajana10@gmail.com>
2024-02-02 07:07:56 +0100

    wip

--- SCHOOL/hodiny/2023/24/firmy_benford.py
+++ SCHOOL/hodiny/2023/24/firmy_benford.py
@@ -11,7 +11,7 @@
 """
 import json
 
-subor = json.load(open("faktury_BB.json"))
+subor = json.load(open("faktury_BB.json", encoding="utf-8"))
 firmy = {}
 for i in range(len(subor)):
     meno = subor[i]["Dodávateľ"]
@@ -38,28 +38,52 @@ for i in range(len(subor)):
                                    "sumy":[cena]}
     else:
         # print("vsetko je, pridavam")
-        """firmy[ico] = {
-            "meno":meno,
-            "data":{
-                rok["cifry"].append(cifra),
-                rok["sumy"].append(cena)
-            }
-        }"""
         firmy[ico]["data"][rok]["cifry"].append(cifra)
         firmy[ico]["data"][rok]["sumy"].append(cena)
 
-"""for ico, x in firmy.items():
-    if ico == "44553625":
-        print(ico, x)"""
-
 fakturanti = {}
+def vyhod_trvale(rok=None):
+    global fakturanti
+    
+    if rok:
+        ico_von = []
+        for ico, udaje in fakturanti.items():
+            platby = udaje["sumy"]
+            for platba in platby:
+                if platby.count(platba) >= len(platby)//2:
+                    ico_von.append(ico)
+                    print("vymazana firma: " + ico)
+                    break
+        for icko in ico_von:
+            del fakturanti[icko]
+
+def rataj_benf(fakturanti):
+    benf = [0.301, 0.176, 0.123, 0.097, 0.079, 0.067, 0.058, 0.051, 0.046]
+
 def vyfiltruj(rok):
     global firmy
     global fakturanti
-    for ico, udaje in firmy.items():
-        if str(rok) in firmy[ico]["data"]:
-            fakturanti[ico] = udaje
+    if rok!="all":
+        for ico, udaje in firmy.items():
+            if int(rok) in firmy[ico]["data"]:
+                fakturanti[ico] = firmy[ico]["data"][int(rok)]
+    # print(fakturanti)
+    elif rok=="all":
+        for ico, udaje in firmy.items():
+            fakturanti[ico] = firmy[ico]["data"]
+    else:
+        print("Neplatný vstup")
+    if rok=="all":
+        vyhod_trvale()
+    else:
+        vyhod_trvale(rok)
 
-for i in fakturanti.values():
-    print(i)
-    
+vyfiltruj(2013)
+
+# print(firmy["44889780"])
+print(firmy["36634654"])  
+# vypisat top 10 spolocnosti, ktore sa vymykaju Benfordovho zakonu
+# -> pomery maju velku odchylku od grafu
+# ak je pocet vyskytu cislice dlhsi ako zoznam(cislic tejto firmy) treba firmu vylucit
+# => nieco ako stale prikazy -> skresluje to benf. krivku
+benf = [0.301, 0.176, 0.123, 0.097, 0.079, 0.067, 0.058, 0.051, 0.046]
